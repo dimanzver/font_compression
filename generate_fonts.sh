a8aa054a058459e5879f9a17306acedfc43b58ed
diman <diman.suvor@yandex.ru>
2020-05-12 13:00:23 +0300

    with include dirs

--- generate_fonts.sh
+++ generate_fonts.sh
@@ -5,9 +5,16 @@ if [ ! -d "dist" ]; then
     mkdir dist
 fi
 
-for filename in $(ls *.ttf)
+for filename in $(ls $1/*.ttf)
 do
 basename=${filename%.*}
+dirname=${filename%/*.*}
+
+if [ ! -d "dist/$dirname" ]; then
+    # Создать папку, только если ее не было
+    mkdir dist/$dirname
+fi
+
 pyftsubset $basename.ttf --output-file=dist/$basename.ttf --unicodes-file=codes.txt
 ttf2woff dist/$basename.ttf dist/$basename.woff
 cat dist/$basename.ttf | ttf2woff2 >> dist/$basename.woff2
